0ac49e9454b70340f7930b318ccff59b78c15908
alamin <alamincsetpi@gmail.com>
2025-05-07 00:28:17 +0600

    check if logout hide the button

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <nav class="bg-gray-800 text-white p-4">
-      <div class="container mx-auto">
-        <router-link to="/" class="mr-4">Home</router-link>
-        <router-link to="/chat">Chat</router-link>
+      <div class="container mx-auto flex justify-between items-center">
+        <div>
+          <router-link to="/" class="mr-4">Home</router-link>
+          <router-link to="/chat">Chat</router-link>
+        </div>
+        <button 
+          v-if="authenticate.isAuthenticated"
+          @click="logout()"
+          class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Logout
+        </button>
       </div>
     </nav>
-     <button 
-         @click="logout()"
-        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Logout
-      </button>
     <div class="container mx-auto p-4">
       <router-view></router-view>
     </div>
@@ -22,10 +25,7 @@
 import { useAuthenticateStore } from '@/stores/authenticate'
 import { useRouter } from 'vue-router'
 
-
 const authenticate = useAuthenticateStore()
-
-
 const router = useRouter()
 //logout
 const logout = () => {
