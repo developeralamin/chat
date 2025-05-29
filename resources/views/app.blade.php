f4dd01259412a3eab87520c34bf46cfa318bd169
alamin <alamincsetpi@gmail.com>
2025-05-29 20:16:42 +0600

    change the csss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
 </head>
 <body>
     <div id="app"></div>
